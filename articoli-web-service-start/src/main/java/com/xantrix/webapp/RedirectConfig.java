7efa3009a9db8dcb217d2a88f20f4f69bdb84c65
umberto <umberto.ornaghi@gmail.com>
2020-10-17 23:56:21 +0200

    Inseriti la genstione dei Log nel file application.yml Commentate le righe di codice che servivano per SSL

--- articoli-web-service-start/src/main/java/com/xantrix/webapp/RedirectConfig.java
+++ articoli-web-service-start/src/main/java/com/xantrix/webapp/RedirectConfig.java
@@ -13,37 +13,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedirectConfig 
 {
-	@Bean
-	public ServletWebServerFactory servletContainer() 
-	{
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() 
-		{
-			@Override
-			protected void postProcessContext(Context context) 
-			{
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				
-				context.addConstraint(securityConstraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(redirectConnector());
-		
-		return tomcat;
-	}
-
-	private Connector redirectConnector() 
-	{
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setPort(5051);
-		connector.setSecure(false);
-		connector.setRedirectPort(5443);
-		
-		return connector;
-	}
+//	@Bean
+//	public ServletWebServerFactory servletContainer() 
+//	{
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() 
+//		{
+//			@Override
+//			protected void postProcessContext(Context context) 
+//			{
+//				SecurityConstraint securityConstraint = new SecurityConstraint();
+//				securityConstraint.setUserConstraint("CONFIDENTIAL");
+//				
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				securityConstraint.addCollection(collection);
+//				
+//				context.addConstraint(securityConstraint);
+//			}
+//		};
+//		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+//		
+//		return tomcat;
+//	}
+//
+//	private Connector redirectConnector() 
+//	{
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//		connector.setScheme("http");
+//		connector.setPort(5051);
+//		connector.setSecure(false);
+//		connector.setRedirectPort(5443);
+//		
+//		return connector;
+//	}
 }
